68e3bb3414b1e9c51813c7203ea697cd4e7972a3
Andrés Filoso <andresfilosok@gmail.com>
2023-07-31 19:41:31 -0300

    add models

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import config from './config';
 import httpClient from './clients/ActivateServiceClient';
 import * as types from './@types';
 import * as schemas from './schemas';
+import * as models from './models';
 
-export { config, helpers, httpClient, types, schemas };
+export { config, helpers, httpClient, types, schemas, models };
 export default {
   config,
   helpers,
   httpClient,
   types,
   schemas,
+  models,
 };
---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+import Cart from './cart';
+import Item from './item';
+import Order from './order';
+
+export { Cart, Order, Item };
+export default { Cart, Order, Item };
