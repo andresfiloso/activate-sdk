009300ea7cf5163df9559c7fe964d18ac41e5daf
Andrés Filoso <andresfilosok@gmail.com>
2023-07-31 19:59:10 -0300

    1.0.8

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ import helpers from './helpers';
 import config from './config';
 import httpClient from './clients/ActivateServiceClient';
 import * as types from './@types';
-import * as schemas from './schemas';
+
 import * as models from './models';
 
-export { config, helpers, httpClient, types, schemas, models };
+export { config, helpers, httpClient, types, models };
 export default {
   config,
   helpers,
   httpClient,
   types,
-  schemas,
   models,
 };
